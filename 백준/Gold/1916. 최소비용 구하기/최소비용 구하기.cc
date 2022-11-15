289e7c87889fb1007129fd7f56d8e70177c10d24
Kunhee Kim <kunheekim@korea.ac.kr>
2022-11-15 16:33:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 116 ms, Memory: 5148 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+#define MAX 20001
+#define INF 1000000000
+using namespace std;
+int v,e;
+vector<pair<int,int>> vertex[MAX];
+int dist[MAX];
+
+void dijkstra(int start){
+    dist[start] = 0;
+    priority_queue<pair<int,int>> pq;
+    pq.push(make_pair(0,start));
+    while(!pq.empty()){
+        int current = pq.top().second;
+        int cost = -pq.top().first;
+        pq.pop();
+        if(dist[current]<cost) continue;
+        for(int i=0; i< vertex[current].size(); i++){
+            int next = vertex[current][i].first;
+            int next_cost = vertex[current][i].second;
+            if(dist[current] + next_cost < dist[next]) {
+               dist[next] = dist[current] + next_cost;
+               pq.push(make_pair(-dist[next],next));
+            }  
+        }
+    }
+}
+
+int main(){
+    cin >> v >> e;
+    int a, b, c;
+    for( int i=0; i<e; i++){
+        cin >> a >> b >> c;
+        vertex[a].push_back(make_pair(b,c));
+    }
+    for(int i=1; i<=v; i++){
+        dist[i] = INF;
+    }
+    int start,end;
+    cin >> start >> end;
+    dijkstra(start);
+    cout << dist[end];
+}
